c4e40aa2f9679b40f74f0c5e6c81271fd8d039d1
ChenRaptor <ChenRaptor@outlook.fr>
2025-02-19 11:13:17 +0100

    fix bug translation julia

--- srcs/julia_utils.c
+++ srcs/julia_utils.c
@@ -6,7 +6,7 @@
 /*   By: abonneau <abonneau@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:51:38 by abonneau          #+#    #+#             */
-/*   Updated: 2025/02/18 14:52:47 by abonneau         ###   ########.fr       */
+/*   Updated: 2025/02/19 11:12:02 by abonneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	compute_const_val_x_julia(t_vars *ctx, t_dvector *const_val_x)
 		offset_x = -(ctx->zoom_x * JULIA_XMAX);
 	const_val_x->x = (rate_x / SCREEN_WIDTH) * ctx->zoom;
 	const_val_x->y = ((rate_x * ctx->center_x) / SCREEN_WIDTH)
-		* ctx->zoom + (ctx->zoom - 1) * 0.5 - offset_x + JULIA_XMIN * ctx->zoom;
+		+ (ctx->zoom - 1) * 0.5 - offset_x + JULIA_XMIN * ctx->zoom;
 }
 
 void	compute_const_val_y_julia(t_vars *ctx, t_dvector *const_val_y)
@@ -32,5 +32,5 @@ void	compute_const_val_y_julia(t_vars *ctx, t_dvector *const_val_y)
 
 	const_val_y->x = (rate_y / SCREEN_HEIGHT) * ctx->zoom;
 	const_val_y->y = ((rate_y * ctx->center_y) / SCREEN_HEIGHT)
-		* ctx->zoom + 1.5 * ctx->zoom_y + JULIA_YMIN * ctx->zoom;
+		+ 1.5 * ctx->zoom_y + JULIA_YMIN * ctx->zoom;
 }
